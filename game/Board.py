fd78cfce6ca1ed95fbcde8409de952fcb9c41887
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-10-22 00:20:42 +0300

    Board and Cell before creating of game's core is done.

--- game/Board.py
+++ game/Board.py
@@ -1,10 +1,26 @@
+from game.CellOfBoard import CellOfBoard
+
+
 class Board(object):
     def __init__(self):
-        pass
+        self.__HORIZONTAL_FRAME = 'ABCDEFGHIJ'
+        self.__VERTICAL_FRAME = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.__DICT_OF_DECK = self.__create_dict_of_board()
 
+    def __create_dict_of_board(self):
+        vertical_frame = tuple(self.__VERTICAL_FRAME)
+        vertical_frame = map(str, vertical_frame)
+        key_dict_of_deck = [[i, j] for i in vertical_frame
+                            for j in self.__HORIZONTAL_FRAME]
+        key_dict_of_deck = tuple(map(','.join, key_dict_of_deck))
+        dict_of_deck = dict()
+        for item in key_dict_of_deck:
+            dict_of_deck[item] = CellOfBoard()
+        return dict_of_deck
 
-    def cell(self):
-        """
+    def get_horizontal_frame(self):
+        return self.__HORIZONTAL_FRAME
 
-        :return:
-        """
+    def get_vertical_frame(self):
+        return self.__VERTICAL_FRAME
+
--- game/CellOfBoard.py
+++ game/CellOfBoard.py
@@ -1,7 +1,15 @@
 class CellOfBoard(object):
     def __init__(self):
-        pass
+        self.__STATUSES_OF_CELL = {'DEFAULT_VALUE': 'о',
+                                   'MISS_SHOOT': 'х',
+                                   'SHIP_IS_DAMAGED': 'К',
+                                   'SHIP_IS_DESTROYED': 'КП'}
+
+        self.__status = self.__STATUSES_OF_CELL.get('DEFAULT_VALUE')
 
     def equal(self, cell):
-        return
+        return self == cell
+
+    def visible(self):
+        return self.__status
 
